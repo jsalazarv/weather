0019ddee8bc95e512ee0f47f4f8db9eb43e475f7
Juan Salazar <jsalazarv8@gmail.com>
2020-09-19 00:30:28 -0500

    Added: map component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-btn class="mt-5 ml-5" fab absolute color="pink" dark @click="setCoordinates">
+        <v-icon>my_location</v-icon>
+      </v-btn>
+      <Map ref="map" :coordinates.sync="coordinates" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Map from "./components/Map";
+
+export default {
+  name: "App",
+  components: {
+    Map
+  },
+  data: () => ({
+    coordinates: {
+      latitude: null,
+      longitude: null
+    }
+  }),
+  methods: {
+    setCoordinates() {
+      this.$refs.map.setCurrentCoordinates();
+    }
+  }
+};
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="map" ref="map"></div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  props: {
+    coordinates: {
+      type: Object,
+      default: () => {
+        return { latitude: null, longitude: null };
+      }
+    }
+  },
+  data: () => ({
+    map: null,
+    coords: { lat: null, lng: null },
+    marker: null
+  }),
+  methods: {
+    init() {
+      const API_KEY = process.env.VUE_APP_GOOGLE_MAPS_API_KEY;
+      const script = document.createElement("script");
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+      script.defer = true;
+
+      window.initMap = () => this.initMap();
+      document.head.appendChild(script);
+    },
+    setCurrentCoordinates() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            this.coords = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            this.$emit("update:coordinates", this.coords);
+          },
+          error => {
+            console.log(error.message);
+          }
+        );
+      } else {
+        console.log("Your browser does not support geolocation API");
+      }
+    },
+    setMapListeners(marker) {
+      google.maps.event.addListener(marker, "dragend", event => {
+        this.coords = {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+        };
+        this.$emit("update:coordinates", this.coords);
+      });
+
+      google.maps.event.addListener(this.map, "click", event => {
+        this.coords = {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+        };
+        this.$emit("update:coordinates", this.coords);
+      });
+    },
+    moveMarker(coords) {
+      this.marker.setPosition(coords);
+    },
+    initMap() {
+      if (this.coordinates.latitude === null || this.coordinates.longitude === null) {
+        this.setCurrentCoordinates();
+      } {
+        this.coords = { lat: this.coordinates.latitude, lng: this.coordinates.longitude };
+      }
+      /*global google */
+      this.map = new google.maps.Map(this.$refs.map, {
+        zoom: 5
+      });
+      this.marker = new google.maps.Marker({
+        map: this.map,
+        draggable: true
+      });
+      this.setMapListeners(this.marker);
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  watch: {
+    coords(newCoordinates) {
+      if(newCoordinates.lat && newCoordinates.lng) {
+        this.moveMarker(newCoordinates);
+        this.map.panTo(newCoordinates);
+      }
+    },
+    coordinates(newCoordinates) {
+      this.coords = { lat: newCoordinates.latitude, lng: newCoordinates.longitude };
+    }
+  }
+};
+</script>
+
+<style scoped>
+#map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
